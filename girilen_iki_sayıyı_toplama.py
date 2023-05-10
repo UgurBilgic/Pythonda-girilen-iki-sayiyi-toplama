478cc37bf91f173140fd5f5aef949966e2a4f5c7
Uğur Bilgiç <ugur31212@gmail.com>
2023-05-10 22:02:20 +0300

    girilen iki sayıyı toplama
    kullanıcının girdiği iki sayıyı toplar

---
+++ girilen_iki_sayıyı_toplama.py
@@ -0,0 +1,15 @@
+#sayı isteme
+
+ilk_sayi = input("First: ")
+ikinci_sayi = input("Second: ")
+
+#sayıları toplama ve float'a çevirme
+
+toplam = float(ilk_sayi) + float(ikinci_sayi)
+
+#toplam değişkenini string'e dönüştürerek ekrana yazdırma
+
+print("Sum:  "+ str(toplam))
+
+input('Çıkış için ENTERa basınız') #bu kodun amacı komut sisteminde komutların tamamlandığı sırada 
+                             #sistemin kendini aniden kapatmamasını sağlar
